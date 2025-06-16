da663bbc8512ea3c2b6ba65ace5e6a97fd03a9f9
Erik Santos <erik.pereira@aluno.fmpsc.edu.br>
2025-06-16 09:21:33 -0300

    Update login.sql

--- avaliacao/login.sql
+++ avaliacao/login.sql
@@ -1,8 +1,14 @@
 CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    senha VARCHAR(255), -- senha com hash
-    foto VARCHAR(255),
-    documento VARCHAR(255)
-);
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    endereco TEXT NOT NULL,
+    foto VARCHAR(255) NOT NULL,
+    pdf VARCHAR(255) NOT NULL,
+    tentativas_login_falhas INT DEFAULT 0,
+    bloqueado_ate DATETIME NULL,
+    reset_token VARCHAR(255) NULL,
+    reset_expires_at DATETIME NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
